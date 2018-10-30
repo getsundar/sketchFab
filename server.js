b61401821b2ef8132c75766d0450354156ec4dfb
Sundhara Senthil Sakthivel <sundharasenthil.sakthivel@klm.com>
2018-10-30 18:16:33 +0100

    Server integration

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 var express = require('express');
 var Collection = require('nedb');
-var annoCollection = new Collection({ filename: 'annotations.json', autoload: true });
+// var annoCollection = new Collection({
+// 	filename: 'annotations.json',
+// 	autoload: true
+// });
 var bodyParser = require("body-parser");
 var app = express();
 var path = require('path');
 var exphbs = require('express-handlebars');
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 app.use(bodyParser.json());
 
 // Webapp, some pages, like the homepage, need to be rendered server side
-var hbs = exphbs.create({extname: '.hbs', partialsDir: path.join(__dirname, '/webapp/customize-partials')});
+var hbs = exphbs.create({
+	extname: '.hbs',
+	partialsDir: path.join(__dirname, '/webapp/customize-partials')
+});
 // So you can use angular templating in HBS files.
-hbs.handlebars.registerHelper('raw-helper', function(options) {
-    return options.fn();
+hbs.handlebars.registerHelper('raw-helper', function (options) {
+	return options.fn();
 });
 
 app.engine('hbs', hbs.engine);
@@ -23,10 +31,10 @@ app.set('views', __dirname);
 app.use(require('./routes'));
 
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  
-  next();
+app.all('/*', function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+
+	next();
 });
 
 app.get('/', function (request, response) {
@@ -34,22 +42,23 @@ app.get('/', function (request, response) {
 })
 
 app.get('/init', function (request, response) {
-	
+
 	annoCollection.count({}, function (error, annoCount) {
 
 		if (annoCount === 0) {
 
-			var defaultAnnotations = [
-				{pox: 2528.731096135685,
-				poy: -19190.43590718395,
-				poz: 3678.676169105742,
-				nox: 0,
-				noy: -0.884616851264307,
-				noz: 0.4663185890131586,
-				camPos: [-2.202090315504301, -52561.6724175868, 16083.3739252877],
-				camTarget: [-2.2020903155, -17439.9461159383, 4671.6332841808],
-				title: 'Defect1',
-				descr: 'This is a test'},
+			var defaultAnnotations = [{
+					pox: 2528.731096135685,
+					poy: -19190.43590718395,
+					poz: 3678.676169105742,
+					nox: 0,
+					noy: -0.884616851264307,
+					noz: 0.4663185890131586,
+					camPos: [-2.202090315504301, -52561.6724175868, 16083.3739252877],
+					camTarget: [-2.2020903155, -17439.9461159383, 4671.6332841808],
+					title: 'Defect1',
+					descr: 'This is a test'
+				},
 				{
 					pox: 11262.413522921077,
 					poy: -19142.15400145214,
@@ -62,10 +71,10 @@ app.get('/init', function (request, response) {
 					title: 'Defect2',
 					descr: 'This is another test'
 				}
-			];		
-			
+			];
 
-			annoCollection.insert( defaultAnnotations );
+
+			annoCollection.insert(defaultAnnotations);
 
 			response.send('Annotation collection created with ' + defaultAnnotations.length + ' new annotations');
 		} else {
@@ -76,32 +85,30 @@ app.get('/init', function (request, response) {
 
 })
 
-app.post('/saveAnnotation', function(req, res){
-  const annotation = req.body;  
-  console.log(annotation);
-  if (annotation) {
-	  annoCollection.insert(annotation, function (err, newDocs) {
-		if(err){
-		  res.status(500).send({'error': err});
-		}else{
-		  res.status(201).send(annotation);
-		}
-	  });
-  } 
+app.post('/saveAnnotation', function (req, res) {
+	const annotation = req.body;	
+	console.log("annotation:::::" + JSON.stringify(annotation.data));
+	if (annotation) {
+		var fs = require('fs');
+		fs.writeFile('annotations.json', JSON.stringify(annotation.data), 'utf8', function (err, obj) {});
+		res.status(201).send(annotation.data);
+	}
 })
 
 app.get('/getAnnotationList', function (request, response) {
-	
+
 	annoCollection.find({}, function (error, annotations) {
-		response.render('webapp/defects', {annos: annotations});
+		response.render('webapp/defects', {
+			annos: annotations
+		});
 	});
 
 })
 
 app.get('/getAnnotations', function (request, response) {
-	
-	annoCollection.find({}, function (error, annotations) {
-		response.send(annotations);
+	var fs = require('fs');
+	fs.readFile('annotations.json', 'utf8', function (err, contents) {
+		response.send(contents);
 	});
 })
 

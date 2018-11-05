39cbb219a340bffb3d23a8bd87234afd91547855
sundar <sundharasenthil.sakthivel@klm.com>
2018-11-05 14:10:08 +0100

    Sketch -fab - changes

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(bodyParser.json());
+app.use(express.static('webapp'));
 
 // Webapp, some pages, like the homepage, need to be rendered server side
 var hbs = exphbs.create({
